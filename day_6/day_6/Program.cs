b665a2118c947bd5c0ca87ac8d19c72b7b6f416d
Modris Ābrams <brainstormer57@googlemail.com>
2018-05-11 21:27:30 +0300

    cikli

--- day_6/day_6/Program.cs
+++ day_6/day_6/Program.cs
@@ -6,7 +6,57 @@ namespace day_6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+
+            /*
+            Console.WriteLine("ievadiet pirmo skaitli:");
+            string ievade1 = Console.ReadLine();
+            double skaitlis1 = Convert.ToInt16(ievade1);
+
+            Console.WriteLine("ievadiet otro skaitli:");
+            string ievade2 = Console.ReadLine();
+            double skaitlis2 = Convert.ToInt16(ievade2);
+
+           double rez = SkaitluSalidzinasana(skaitlis1, skaitlis2);
+
+            Console.WriteLine("lielakais skaitlis ir " + rez);
+
+
+            Console.ReadLine();
+            */
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(i);
+            }
+            int skaititajs = 0;
+            while(skaititajs != 5)
+            {
+                Console.WriteLine(skaititajs);
+                skaititajs++;
+            }
+
+
+
+            Console.ReadLine();
         }
+
+        //static double SkaitluSalidzinasana(double a, double b)
+
+
+        /*
+    double rezultats;
+
+    if (a > b) 
+    {
+        rezultats = a;
     }
+    else
+    {
+        rezultats = b;
+    }
+
+    return rezultats;
+    */
+    }   
 }
